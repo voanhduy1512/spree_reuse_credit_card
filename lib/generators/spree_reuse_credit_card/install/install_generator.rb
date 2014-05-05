3b820ae8d8361094a4483bee8a17970efaac6900
Jonathan Tapia <jonathan.tapia@crowdint.com>
2014-05-05 10:41:58 -0500

    Move assets to correct folder root

--- lib/generators/spree_reuse_credit_card/install/install_generator.rb
+++ lib/generators/spree_reuse_credit_card/install/install_generator.rb
@@ -3,13 +3,13 @@ module SpreeReuseCreditCard
     class InstallGenerator < Rails::Generators::Base
 
       def add_javascripts
-        append_file "app/assets/javascripts/store/all.js", "//= require store/spree_reuse_credit_card\n"
-        append_file "app/assets/javascripts/admin/all.js", "//= require admin/spree_reuse_credit_card\n"
+        append_file "app/assets/javascripts/spree/frontend/all.js", "//= require spree/frontend/spree_reuse_credit_card\n"
+        append_file "app/assets/javascripts/spree/backend/all.js", "//= require spree/backend/spree_reuse_credit_card\n"
       end
 
       def add_stylesheets
-        inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/spree_reuse_credit_card\n", :before => /\*\//, :verbose => true
-        inject_into_file "app/assets/stylesheets/admin/all.css", " *= require admin/spree_reuse_credit_card\n", :before => /\*\//, :verbose => true
+        inject_into_file "app/assets/stylesheets/spree/frontend/all.css", " *= require spree/frontend/spree_reuse_credit_card\n", :before => /\*\//, :verbose => true
+        inject_into_file "app/assets/stylesheets/spree/backend/all.css", " *= require spree/backend/spree_reuse_credit_card\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
