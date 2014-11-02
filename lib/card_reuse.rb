029b6b6ad3a732956c5bf71493e85a5d06b73ce9
Võ Anh Duy <voanhduy1512@live.com>
2014-11-02 10:56:59 +0700

    use relation credit_cards on users instead of payments

--- lib/card_reuse.rb
+++ lib/card_reuse.rb
@@ -2,17 +2,8 @@ module CardReuse
   def all_cards_for_user(user)
     return nil unless user
 
-    payments = credit_card_payments_for_user(user)
-
-    payments.map do |payment|
-      src = payment.source
-
-      if valid_for_reuse?(src)
-        src
-      else
-        nil
-      end
-    end.compact.uniq
+    credit_cards = user.credit_cards
+    credit_cards.delete_if { |card| valid_for_reuse? card }
   end
 
   def card_expired?(payment_source)
@@ -25,9 +16,5 @@ module CardReuse
   def valid_for_reuse?(payment_source)
     # some payment gateways use only one of these?  stripe possibly?
     !(payment_source.nil? || (payment_source.gateway_payment_profile_id.nil? && payment_source.gateway_customer_profile_id.nil?) || payment_source.deleted? || card_expired?(payment_source))
-  end
-
-  def credit_card_payments_for_user(user)
-    Spree::Payment.joins(:order).where('spree_orders.completed_at IS NOT NULL').where('spree_orders.user_id' => user.id).order('spree_orders.created_at').where('spree_payments.source_type' => 'Spree::CreditCard').where('spree_payments.state' => 'completed')
   end
 end
